4cd0533bc78a80ded5a59013e8b37fc8e49c412e
TomasBlanco01 <TomasblancooUTN@gmail.com>
2025-10-15 09:36:01 -0300

    configurando 05 para subir a Render

--- controllers/avisosController.js
+++ controllers/avisosController.js
@@ -85,9 +85,17 @@ const createAviso = async (req, res) => {
 const deleteAviso = async (req, res) => {
   try {
     const result = await pool.query('DELETE FROM avisos WHERE id = $1 RETURNING *', [req.params.id]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "Aviso no encontrado" });
+    }
+
+    res.status(200).json({ message: "Aviso eliminado", aviso: result.rows[0] });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error al eliminar aviso" });
-  }};
+  }
+};
+
 
 export { getAvisos, getAvisoById, createAviso, deleteAviso };
