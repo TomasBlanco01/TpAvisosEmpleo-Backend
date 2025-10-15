49d088b857ebf832e5c893f833fc7d3a3e688e7a
TomasBlanco01 <TomasblancooUTN@gmail.com>
2025-10-14 22:07:12 -0300

    configuracion para subir a Render

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,18 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+import pkg from 'pg';
+const { Pool } = pkg;
+/*const { Pool } = require('pg');
+require('dotenv').config();*/
 
 const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+  /*
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
+  */
 });
 
 pool.connect()
